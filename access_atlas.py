91d8cec74de6b5ce8bfd3caaa193936a4b2c3e7c
Tam Nguyen <npt.dc@outlook.com>
2021-09-18 18:34:01 +0700

    🔧 Update Atlas access example

--- access_atlas.py
+++ access_atlas.py
@@ -8,7 +8,7 @@ conn_str = config["mongodb_dev"]
 client = MongoClient(conn_str)
 print(client.list_database_names())
 
-db = client["db"]
+db = client["flow-backoffice"]
 print(db.list_collection_names())
 
 issuer = db.Issuer.find_one()
